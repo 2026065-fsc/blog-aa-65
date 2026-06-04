e5f19b90e4521813ca1b2816f50479f1ffd88978
Imaoka <=2026065@fsc-educ.com>
2026-06-04 14:59:54 +0900

    Service作成

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -1,7 +1,14 @@
+package com.example.blog_app;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BlogController {
+
+    private final BlogService blogService;
+
+    public BlogController(BlogService blogService){
+        this.blogService = blogService;
+    }
     @GetMapping("path")
     public String getMethodName(@RequestParam String param) {
         return new String();
---
+++ src/main/java/com/example/blog_app/BlogService.java
@@ -0,0 +1,9 @@
+package com.example.blog_app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogService {
+    
+    
+}
