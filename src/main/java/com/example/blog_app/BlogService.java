3862c7a3b695b68880e84f9b40a4452139036470
2026065-fsc <2026065@fsc-educ.com>
2026-06-12 02:04:54 +0900

    Update BlogService.java

--- src/main/java/com/example/blog_app/BlogService.java
+++ src/main/java/com/example/blog_app/BlogService.java
@@ -19,15 +19,15 @@ public class BlogService {
     public Blog getById(long id){    // 詳細
         return blogRepository.findById(id);
     }
-
+    public List<Blog> getBlogsByAuthor(long authorId){    // アカウントID検索
+        return blogRepository.findByAuthorId(authorId);
+    }
     public List<Blog> searchByKeyword(String keyword){    // キーワード検索
         return blogRepository.findByKeyword(keyword);
     }
-
     public List<Blog> searchByCategory(String category){    // カテゴリ検索
         return blogRepository.findByCategory(category);
     }
-
     public List<Blog> searchByAuthor(String author){    // 作者検索
         return blogRepository.findByAuthor(author);
     }
