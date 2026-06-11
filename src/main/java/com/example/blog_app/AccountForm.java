dbb13d04796a1293161d80a5d078960d2d702a53
2026065-fsc <2026065@fsc-educ.com>
2026-06-11 21:56:25 +0900

    Create AccountForm.java

---
+++ src/main/java/com/example/blog_app/AccountForm.java
@@ -0,0 +1,29 @@
+package com.example.blog_app;
+
+public class AccountForm {  //フォーム入力
+
+    private String username;
+    private String password;
+    private String profileText;
+
+    public String getUsername() { 
+      return username;
+    }
+    public void setUsername(String username) { 
+      this.username = username;
+    }
+
+    public String getPassword() { 
+      return password;
+                                }
+    public void setPassword(String password) { 
+      this.password = password;
+    }
+
+    public String getProfileText() { 
+      return profileText;
+                                   }
+    public void setProfileText(String profileText) { 
+      this.profileText = profileText;
+    }
+}
